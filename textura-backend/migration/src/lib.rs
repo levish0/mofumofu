3191b138d13fa7a6b2911504bb911f4df589c392
levish0 <shiueo.csh@gmail.com>
2025-07-17 15:50:47 +0900

    migration

---
+++ textura-backend/migration/src/lib.rs
@@ -0,0 +1,13 @@
+pub use sea_orm_migration::prelude::*;
+mod m20250717_064916_create_users;
+
+pub struct Migrator;
+
+#[async_trait::async_trait]
+impl MigratorTrait for Migrator {
+    fn migrations() -> Vec<Box<dyn MigrationTrait>> {
+        vec![
+            Box::new(m20250717_064916_create_users::Migration),
+        ]
+    }
+}
