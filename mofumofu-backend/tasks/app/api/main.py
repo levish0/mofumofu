7ff0ae2e24e75307027b3ab9cf537526f30f24cb
levish0 <shiueo.csh@gmail.com>
2025-08-03 23:36:39 +0900

    디렉터리 스트럭쳐 정리

--- mofumofu-backend/tasks/app/api/main.py
+++ mofumofu-backend/tasks/app/api/main.py
@@ -1,23 +1,19 @@
 from fastapi import APIRouter
-from app.api.routes.oauth import router as oauth_router
-from app.api.routes.avatar import router as avatar_router
-from app.api.routes.banner import router as banner_router
-from app.api.routes.common import router as common_router
-from app.api.routes.token_cleanup import router as token_cleanup_router
+from app.api.routes.user.oauth import router as oauth_router
+from app.api.routes.user.avatar import router as avatar_router
+from app.api.routes.user.banner import router as banner_router
+from app.api.routes.common.status import router as status_router
+from app.api.routes.token.cleanup import router as token_cleanup_router
 
 api_router = APIRouter()
 
-# OAuth 관련 라우터 추가
+# 사용자 관련 라우터 추가
 api_router.include_router(oauth_router)
-
-# 아바타 관련 라우터 추가
 api_router.include_router(avatar_router)
-
-# 배너 관련 라우터 추가
 api_router.include_router(banner_router)
 
 # 공통 기능 라우터 추가
-api_router.include_router(common_router)
+api_router.include_router(status_router)
 
-# 토큰 정리 관련 라우터 추가
+# 토큰 관련 라우터 추가
 api_router.include_router(token_cleanup_router)
