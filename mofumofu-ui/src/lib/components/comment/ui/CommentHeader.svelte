e8812563c77b1971e06fc671da83e0bfbc65227e
levish0 <shiueo.csh@gmail.com>
2025-08-20 17:56:50 +0900

    Update CommentHeader.svelte

--- mofumofu-ui/src/lib/components/comment/ui/CommentHeader.svelte
+++ mofumofu-ui/src/lib/components/comment/ui/CommentHeader.svelte
@@ -30,7 +30,10 @@
 
 <div class="flex items-center gap-2 text-sm">
 	{#if !isDeleted}
-		<a href="/u/{displayUserHandle}" class="text-mofu-light-200 dark:text-mofu-dark-200 font-medium hover:underline">
+		<a
+			href="/@{displayUserHandle}/profile"
+			class="text-mofu-light-200 dark:text-mofu-dark-200 font-medium hover:underline"
+		>
 			{displayUserName}
 		</a>
 	{:else}
